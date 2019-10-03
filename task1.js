2b5b2619dfd0525f061a209452b11463efd340ee
kristopher1robin <kristopher1robin@gmail.com>
2019-10-03 12:15:44 +0300

    add task1.js

---
+++ task1.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const percentile = require('percentile');
+const math = require('mathjs');
+
+let nodePath = process.argv[0];
+let appPath = process.argv[1];
+let numfile = process.argv[2];
+
+fs.readFile(numfile, 'utf8', (err, content) => {
+
+    let arr = [];
+
+    if (err) throw err;
+
+    let array = content.toString().split("\n");
+    for (i in array) {
+        arr.push(array[i]);
+    }
+
+    let result = [];
+
+    result.push(Number.parseInt(percentile(90, arr)));
+    result.push(math.median(arr));
+    result.push(Math.max.apply(null, arr));
+    result.push(Math.min.apply(null, arr));
+    result.push(math.mean(arr));
+
+    if (result[2] > 32767 || result[3] < -32768) {
+        console.log('Must be [-32768, 32767)');
+    } else {
+        /* форматировать и вывести результаты */
+        console.log(Math.round(result * 100));
+    }
+
+});
